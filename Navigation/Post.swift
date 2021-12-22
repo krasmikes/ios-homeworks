b181b0bc75aa9dfe0127d5457de992dabbe10f48
Апанасенко Михаил Витальевич <mvapanasenko@domclick.ru>
2021-12-22 23:27:27 +0300

    Added tab bar navigation and several screens

---
+++ Navigation/Post.swift
@@ -0,0 +1,12 @@
+//
+//  Post.swift
+//  Navigation
+//
+//  Created by Апанасенко Михаил Витальевич on 22.12.2021.
+//
+
+import Foundation
+
+struct Post {
+    let title: String
+}
--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -13,10 +13,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+          
+        let window = UIWindow(windowScene: windowScene)
+        
+        let feedNavigationController = UINavigationController(rootViewController: FeedViewController())
+        feedNavigationController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "newspaper"), tag: 0)
+        
+        let profileNavigationController = UINavigationController(rootViewController: ProfileViewController())
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 1)
+        
+        let tabBarViewController = UITabBarController()
+        tabBarViewController.viewControllers = [feedNavigationController, profileNavigationController]
+        
+        window.rootViewController = tabBarViewController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
