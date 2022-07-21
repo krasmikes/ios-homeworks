ef0c08017a9e22a214a827b3715cee8bb3418602
Mikhail Apanasenko <43650260+krasmikes@users.noreply.github.com>
2022-07-21 13:26:08 +0400

    Revert "Решение задания 4"

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Апанасенко Михаил Витальевич on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
